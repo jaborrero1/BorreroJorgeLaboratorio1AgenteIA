7149552a42fc2a2a35fd30dd6b8060960365a36e
jaborrero1 <jorgerey2011@gmail.com>
2022-05-25 01:24:13 -0500

    Ingreso de ubicacion Puesto 1

--- maquinadecoser.py
+++ maquinadecoser.py
@@ -14,3 +14,17 @@ def maquina_coser():
     # El confeccionista indica si la maquina de coser se encuentra realizando buenas puntadas 
     # o malas puntadas (0/1) en la ubicación señalada.
     ingresar_estado = input("Ingreso de la ubicacion : " +ingresar_ubicacion+ " ")
+
+    #El confeccionista ingresa el Puesto1
+    if (ingresar_ubicacion == 'Puesto1'):
+        
+        # el usuario indica si la maquina de coser se encuentra realizando una buena puntadas 
+        # o mala puntada (0/1) en la ubicación señalada.
+        estado_ubicacion2 = input("Ingreso del estado del puesto 2: ")
+        estado_ubicacion3 = input("Ingreso del estado del puesto 3: ")
+
+        #Se imprime el resultado obtenido
+        print("Destino Deseado Obtenido:" + str(estado_objetivo))
+
+        # Se imprime en que puesto esta ubicado la maquina de coser
+        print("La maquina de coser se encuentra en el puesto 1")
