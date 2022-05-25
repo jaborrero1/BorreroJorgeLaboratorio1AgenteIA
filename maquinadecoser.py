95c9d173ce4becf196ad345f4c1c73b50578cbaf
jaborrero1 <jorgerey2011@gmail.com>
2022-05-25 05:08:15 -0500

    Ingreso del Puesto 2 - ingreso ubicacion 3

--- maquinadecoser.py
+++ maquinadecoser.py
@@ -218,3 +218,25 @@ def maquina_coser():
                 #La maquina de coser se encuentra realizando buenas puntadas
                 print("La maquina de coser del Puesto 1 se encuentra realizando buenas puntadas")
                 print("No se realiza ninguna acción. El costo actual es de: " + str(costo))
+                if(estado_ubicacion3 == '1'):
+                    #Si la maquina de coser del Puesto 3 se encuentra en malas puntadas
+                    print("La maquina de coser del Puesto 3 se encuentra realizando malas puntadas")
+                    print("La maquina de coser esta ubicado en el Puesto 3")
+                    #Se incrementa el costo de la ubicacion de la maquina de coser
+                    costo += 1
+                    #Se obtiene el costo actual
+                    print("El costo actual es de: " + str(costo)) 
+                    #La maquina de coser se encuentra realizando buenas puntadas
+                    estado_objetivo['Puesto3'] ='0'
+                    #El costo ira incrementado cada que la maquina de coser realice buenas puntadas
+                    costo += 1 
+                    print("La maquina de coser se encuentra relizando buenas puntadas en el Puesto 3")
+                    #Se obtiene el costo actual 
+                    print("El costo actual es de: " + str(costo)) 
+                else:
+                    #La maquina de coser se encuentra realizando buenas puntadas
+                    print("La maquina de coser del Puesto 3 se encuentra realizando buenas puntadas")
+                    print("No se realiza ninguna acción. El costo actual es: " + str(costo))
+
+        else:      
+            print("El estado ingresado no es el correcto")
