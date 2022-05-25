216d9a140fd9c02f0fc5811cf0099e497a60154b
jaborrero1 <jorgerey2011@gmail.com>
2022-05-25 00:57:05 -0500

    variables

--- maquinadecoser.py
+++ maquinadecoser.py
@@ -1,3 +1,16 @@
 #INSTRUCCIONES
 #Ingreso de Ubicaciones Puesto1 - Puesto2 - Puesto3
 #Ingreso del estado 0 / 1, donde 0 significa buenas puntadas y 1 malas puntadas
+
+def maquina_coser():
+    #Inicializacion del estado objectivo
+    # 0 indica Buenas puntadas y 1 malas puntadas
+    estado_objetivo = {'Puesto1': '0', 'Puesto2': '0', 'Puesto3': '0'}
+    costo = 0
+
+    #El confeccionista indica la ubicación de la maquina de coser
+    ingresar_ubicacion = input("Ingreso de la ubicacion de la maquina de coser: ")
+
+    # El confeccionista indica si la maquina de coser se encuentra realizando buenas puntadas 
+    # o malas puntadas (0/1) en la ubicación señalada.
+    ingresar_estado = input("Ingreso de la ubicacion : " +ingresar_ubicacion+ " ")
